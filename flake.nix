a3f126f91a7eea698966a390f3f2e87db52072b9
Eva Emmerich <eva@evaemmerich.com>
2025-11-09 18:04:01 +0100

    add nix build env

---
+++ flake.nix
@@ -0,0 +1,44 @@
+{
+  description = "librespot development flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs:
+  let
+
+    # allows us to use the same devShells/package/etc definitions for multiple architectures
+    # borrowed from https://ayats.org/blog/no-flake-utils
+    forAllSystems = function:
+      nixpkgs.lib.genAttrs [
+        "x86_64-linux"
+        "aarch64-linux"
+      ] (system: function nixpkgs.legacyPackages.${system});
+
+  in {
+
+    devShells = forAllSystems (pkgs: {
+      default = pkgs.mkShell {
+        nativeBuildInputs = with pkgs; [
+          rustc
+          cargo
+          gcc
+          rustfmt
+          clippy
+          rustPlatform.cargoSetupHook
+          openssl
+          pkg-config
+          alsa-lib
+        ];
+        buildInputs = with pkgs; [
+        ];
+
+        # Certain Rust tools won't work without this
+        # This can also be fixed by using oxalica/rust-overlay and specifying the rust-src extension
+        # See https://discourse.nixos.org/t/rust-src-not-found-and-other-misadventures-of-developing-rust-on-nixos/11570/3?u=samuela. for more details.
+        RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+      };
+    });
+  };
+}
